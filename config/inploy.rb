48550546eb07d6f77553780096729a617b4b8d84
Stefan Botzenhart <sb@basiszwo.com>
2010-11-09 14:42:49 +0100

    add inploy.rb to gitignore; change sample inploy config;

--- config/inploy.rb
+++
@@ -1,9 +0,0 @@
-application = "signal"
-repository = 'git://github.com/dcrec1/signal.git'
-user = 'dcrec1'
-hosts = ['hooters', 'geni']
-path = '/opt'
-
-before_restarting_server do
-  run "script/delayed_job -e production restart"
-end
